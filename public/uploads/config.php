7fe7cdf9775abb3c379b8a0d9f5563f43e49327e
Neil Jennings <neiljennings@ntlworld.com>
2016-03-03 11:02:40 +0000

    add empty directories

---
+++ public/uploads/config.php
@@ -0,0 +1,5 @@
+<?php
+
+filemanager_set_permissions();
+$filePermissions['system']['prevent_delete'] = true;
+$filePermissions['system']['prevent_rename'] = true;
---
+++ public/uploads/system/config.php
@@ -0,0 +1,4 @@
+<?php
+
+filemanager_set_permissions(false);
+$folder_message = "Note: system files can't be directly removed";
