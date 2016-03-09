665481832bd60a35323034832c651cdc50ac118e
Neil Jennings <neiljennings@ntlworld.com>
2016-03-09 11:49:54 +0000

    add image block to default theme

--- resources/views/themes/default/templates/internal.blade.php
+++ resources/views/themes/default/templates/internal.blade.php
@@ -5,7 +5,14 @@
         <div class="row">
             <div class="col-sm-12">
                 <h2>{!! PageBuilder::block('title') !!}</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-9">
                 {!! PageBuilder::block('content') !!}
+            </div>
+            <div class="col-sm-3">
+                {!! PageBuilder::block('image') !!}
             </div>
         </div>
         <!-- /.row -->
