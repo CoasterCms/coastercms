1c8efeb4ca8aba874d75be418bd69c3e918842cc
Neil Jennings <neiljennings@ntlworld.com>
2016-04-12 11:44:43 +0100

    relative filemanager urls

--- public/coaster/filemanager/config/config.php
+++ public/coaster/filemanager/config/config.php
@@ -137,7 +137,7 @@ $config = array(
 	|
 	*/
 
-	'base_url' => ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && ! in_array(strtolower($_SERVER['HTTPS']), array( 'off', 'no' ))) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'],
+	'base_url' => '',
 
 	/*
 	|--------------------------------------------------------------------------
