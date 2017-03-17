cfd24d4e05899ee4fabe8eab6a7c96ec54b3546c
Neil Jennings <neil-jennings@users.noreply.github.com>
2017-03-17 16:39:42 +0000

    routes provider

--- config/app.php
+++ config/app.php
@@ -176,6 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+    
+        /*
+         * Package Service Providers...
+         */
+        CoasterCms\Providers\CoasterRoutesProvider::class,
 
     ],
 
