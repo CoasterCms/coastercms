511bbdda2e0acae2fa0f5aa2ea0747d71ec5d388
Neil Jennings <neil@web-feet.co.uk>
2017-06-02 10:23:41 +0100

    laravel 5.4 updates

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Coaster CMS',
+    'name' => env('APP_NAME', 'Coaster CMS'),
 
     /*
     |--------------------------------------------------------------------------
